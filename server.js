cb79c074314c516074513ef48e4b22427cd111c1
sterling <sterlingsrq@gmail.com>
2015-10-05 16:37:44 -0400

    Update server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var gzippo  = require('gzippo'),
     app     = express(),
     port    = process.env.PORT || 3000;
 
-app.use(gzippo.staticGzip('public/'));
+app.use(gzippo.staticGzip(""  + __dirname + '/public/'));
 
 app.listen(port);
